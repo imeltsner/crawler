671a7409c02715c19c470abc577228abac36c8d8
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-04 13:23:11 -0700

    update normalize_url

--- normalize_url.go
+++ normalize_url.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
 func normalizeURL(inputURL string) (string, error) {
-	return "", nil
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
+	}
+
+	fullPath := parsedURL.Host + parsedURL.Path
+
+	fullPath = strings.ToLower(fullPath)
+
+	fullPath = strings.TrimSuffix(fullPath, "/")
+
+	return fullPath, nil
 }
