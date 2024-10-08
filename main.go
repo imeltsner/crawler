4ab3f86bf7bb34aa62e69916dea4d01548c7be12
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-07 17:24:38 -0700

    added basic cmd line parsing

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 2 {
+		fmt.Printf("too many arguments provided")
+		os.Exit(1)
+	}
+
+	fmt.Printf("starting crawl of: %v", args[1])
 }
