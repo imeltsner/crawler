29227e64a72fb773acaef24548fd3070353b5bc4
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-07 17:02:30 -0700

    added unit tests for getURLsFromHTML

---
+++ get_urls_test.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetURLsFromHTML(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputURL  string
+		inputBody string
+		expected  []string
+	}{
+		{
+			name:     "absolute and relative URLs",
+			inputURL: "https://blog.boot.dev",
+			inputBody: `
+	<html>
+		<body>
+			<a href="/path/one">
+				<span>Boot.dev</span>
+			</a>
+			<a href="https://other.com/path/one">
+				<span>Boot.dev</span>
+			</a>
+		</body>
+	</html>
+	`,
+			expected: []string{"https://blog.boot.dev/path/one", "https://other.com/path/one"},
+		},
+		{
+			name:     "absolute only",
+			inputURL: "https://blog.boot.dev",
+			inputBody: `
+	<html>
+		<body>
+			<a href="https://xyz.com/path/one">
+				<span>Boot.dev</span>
+			</a>
+			<a href="https://other.com/path/one">
+				<span>Boot.dev</span>
+			</a>
+		</body>
+	</html>
+	`,
+			expected: []string{"https://xyz.com/path/one", "https://other.com/path/one"},
+		},
+		{
+			name:     "relative only",
+			inputURL: "https://blog.boot.dev",
+			inputBody: `
+	<html>
+		<body>
+			<a href="/path/one">
+				<span>Boot.dev</span>
+			</a>
+			<a href="/path/two">
+				<span>Boot.dev</span>
+			</a>
+		</body>
+	</html>
+	`,
+			expected: []string{"https://blog.boot.dev/path/one", "https://blog.boot.dev/path/two"},
+		},
+		{
+			name:     "three mixed only",
+			inputURL: "https://blog.boot.dev",
+			inputBody: `
+	<html>
+		<body>
+			<a href="/path/one">
+				<span>Boot.dev</span>
+			</a>
+			<a href="https://xyz.com/path">
+				<span>Boot.dev</span>
+			</a>
+			<a href="/path/two">
+				<span>Boot.dev</span>
+			</a>
+		</body>
+	</html>
+	`,
+			expected: []string{"https://blog.boot.dev/path/one", "https://xyz.com/path", "https://blog.boot.dev/path/two"},
+		},
+		{
+			name:      "empty html",
+			inputURL:  "https://blog.boot.dev",
+			inputBody: "",
+			expected:  nil,
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := getURLsFromHTML(tc.inputBody, tc.inputURL)
+
+			if err != nil {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			}
+
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Test %v - %s FAIL: expected: %v, actual: %v", i, tc.name, tc.expected, actual)
+			}
+		})
+	}
+}
