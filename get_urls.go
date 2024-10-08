b960caa60ecf1a7fac003ae3fdb9cc81d8dcd7d2
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-07 17:03:08 -0700

    added getURLsFromHTML function

--- get_urls.go
+++ get_urls.go
@@ -1,3 +1,45 @@
 package main
 
-func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error)
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	htmlReader := strings.NewReader(htmlBody)
+
+	nodes, err := html.Parse(htmlReader)
+	if err != nil {
+		fmt.Errorf("unable to parse html %w", err)
+		return []string{}, nil
+	}
+
+	var links []string
+
+	var parseLinkData = func(attr []html.Attribute) {
+		for _, attr := range attr {
+			if attr.Key == "href" {
+				if !strings.Contains(attr.Val, "https://") {
+					attr.Val = rawBaseURL + attr.Val
+				}
+				links = append(links, attr.Val)
+				return
+			}
+		}
+	}
+
+	var getLinks func(node *html.Node)
+	getLinks = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			parseLinkData(node.Attr)
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			getLinks(child)
+		}
+	}
+
+	getLinks(nodes)
+	return links, nil
+}
