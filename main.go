c35ef922ea7582f8eecc4e99fd04fe6d18746d08
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-10-08 18:24:46 -0700

    added crawlPage to main

--- main.go
+++ main.go
@@ -10,13 +10,15 @@ func main() {
 
 	if len(args) < 2 {
 		fmt.Printf("no website provided")
-		os.Exit(1)
+		return
 	}
 
 	if len(args) > 2 {
 		fmt.Printf("too many arguments provided")
-		os.Exit(1)
+		return
 	}
 
-	fmt.Printf("starting crawl of: %v", args[1])
+	var pages = map[string]int{}
+	fmt.Printf("starting crawl of: %v\n", args[1])
+	crawlPage(args[1], args[1], pages)
 }
